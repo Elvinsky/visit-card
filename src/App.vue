3df60f0bacf3f1f46c2017389336c5764299dad7
Elvinsky <mikhnevichnik@gmail.com>
2024-02-13 09:30:50 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-row">
+    <HelloWorld />
+  </div>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<style lang="scss" scoped></style>
+
