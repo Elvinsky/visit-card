6eb2a84f6def0fc3510a37f432c6c5e9cd016dc6
Elvinsky <mikhnevichnik@gmail.com>
2024-02-14 14:01:46 +0100

    feat: set up basic vars for project

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "./src/styles/basics/_media.scss" as *;`,
+      },
+    },
+  },
   plugins: [
     vue(),
   ],
